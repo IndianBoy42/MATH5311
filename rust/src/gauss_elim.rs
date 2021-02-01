b326a7a691654c44daa163c5242fe3401964dbff
Anshuman <amedhi@connect.ust.hk>
2021-02-01 18:19:03 +0800

    try force unroll/autovectorize in part 2 of gauss_elim

--- rust/src/gauss_elim.rs
+++ rust/src/gauss_elim.rs
@@ -1,6 +1,6 @@
-use ndarray::{Axis, IntoNdProducer, azip, par_azip, s};
+use ndarray::{azip, par_azip, s, Array, Array1, Axis, IntoNdProducer};
 
-use std::ops::{DivAssign, SubAssign, MulAssign};
+use std::ops::{DivAssign, MulAssign, SubAssign};
 
 pub fn gauss_elim(mut A: ndarray::ArrayViewMut2<f32>, mut B: ndarray::ArrayViewMut2<f32>) {
     assert_eq!(A.nrows(), B.nrows());
@@ -11,8 +11,8 @@ pub fn gauss_elim(mut A: ndarray::ArrayViewMut2<f32>, mut B: ndarray::ArrayViewM
     for k in 0..N {
         let a = A[(k, k)];
         let (mut a_blk, mut a_col, a_row) = A.multi_slice_mut((
-            s![(k + 1).., (k + 1)..],
-            s![(k + 1).., k],
+            s![(k + 1).., (k + 1)..], //.
+            s![(k + 1).., k],         //.
             s![k, (k + 1)..],
         ));
         let a_row = a_row.view();
@@ -20,7 +20,7 @@ pub fn gauss_elim(mut A: ndarray::ArrayViewMut2<f32>, mut B: ndarray::ArrayViewM
         let (bk, mut brest) = B.multi_slice_mut((s![k, ..], s![(k + 1).., ..]));
         let bk = bk.view();
 
-        par_azip!((mut lrow in a_blk.genrows_mut(), r in a_col, lhs in brest.genrows_mut()) {
+        azip!((mut lrow in a_blk.genrows_mut(), r in a_col, lhs in brest.genrows_mut()) {
             *r/=a;
             let r = *r;
             azip!((l in lrow, &c in a_row) {
@@ -37,8 +37,14 @@ pub fn gauss_elim(mut A: ndarray::ArrayViewMut2<f32>, mut B: ndarray::ArrayViewM
         let (b_nxt, mut b_row) = B.multi_slice_mut((s![(k + 1).., ..], s![k, ..]));
         let akk = A[(k, k)];
 
-        par_azip!((bcol in b_nxt.gencolumns(), bk in b_row) {
-            let ab = bcol.dot(&a);
+        azip!((bcol in b_nxt.gencolumns(), bk in b_row) {
+            const UNROLL: usize = 32;
+            let ab: f32 = bcol.axis_chunks_iter(Axis(0), UNROLL)
+                .zip(a.axis_chunks_iter(Axis(0), UNROLL))
+                .map(|(a,b)|a.dot(&b)).sum();
+                // .fold(0f32, |acc, (a,b)| acc + a.dot(&b));
+                // .fold(0f32, |acc, (a,b)| acc + (a.to_owned()*b).sum());
+            // let ab = bcol.dot(&a);
             *bk = (*bk - ab) / akk;
         })
     }
